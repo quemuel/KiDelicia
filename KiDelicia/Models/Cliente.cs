40137fa5c318bf1a0e840aa8683ae976eb349e5c
kprofissa - casa <kemuel_pereira@yahoo.com.br>
2018-08-04 19:53:44 -0300

    Ajustes nos models; Ajustes nos controllers e views.

--- KiDelicia/Models/Cliente.cs
+++ KiDelicia/Models/Cliente.cs
@@ -9,17 +9,18 @@ namespace KiDelicia.Models
     public class Cliente
     {
         [Key]
-        public int IdCliente { get; set; }
+        public int ClienteId { get; set; }
 
         [Required, StringLength(300)]
-        public string Nome { get; set; }
+        public string NomeCliente { get; set; }
 
         [Required, StringLength(500)]
-        public string Endereco { get; set; }
+        public string EnderecoCliente { get; set; }
 
         [Required, StringLength(11)]
-        public string Telefone { get; set; }
+        public string TelefoneCliente { get; set; }
 
         public virtual ICollection<ConsumoComanda> ConsumoComandas { get; set; }
+        public virtual ICollection<BaixaMes> BaixaMeses { get; set; }
     }
 }
