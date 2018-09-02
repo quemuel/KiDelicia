34ef558c62e36cc12cc6659201f8631771071376
Ediva Goncalves Teodozio <edivaa@gmail.com>
2018-09-01 23:37:15 -0300

    Criacao PerfilController

---
+++ KiDelicia/Controllers/PerfilController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KiDelicia.Controllers
+{
+    public class PerfilController : Controller
+    {
+        // GET: Perfil
+        [Authorize]
+        public ActionResult AlterarSenha()
+        {
+            return View();
+        }
+    }
+}
