f4125fad790edbefe700dd6a88435027c06747c4
Ediva Goncalves Teodozio <edivaa@gmail.com>
2018-09-01 23:36:40 -0300

    ViewModel para campos de alteracao de senha

---
+++ KiDelicia/ViewModels/AlterarSenhaViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KiDelicia.ViewModels
+{
+    public class AlterarSenhaViewModel
+    {
+        [Required(ErrorMessage = "Informe sua senha atual")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha atual")]
+        [MinLength(6, ErrorMessage = "A senha deve ter pelo menos 6 caracteres")]
+        public string SenhaAtual { get; set; }
+
+        [Required(ErrorMessage = "Informe sua senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Nova senha")]
+        [MinLength(6, ErrorMessage = "A senha deve ter pelo menos 6 caracteres")]
+        public string NovaSenha { get; set; }
+
+        [Required(ErrorMessage = "Informe sua senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confime sua senha")]
+        [MinLength(6, ErrorMessage = "A senha deve ter pelo menos 6 caracteres")]
+        [Compare(nameof(NovaSenha), ErrorMessage = "A senha e a confirmaço nao estao iguais")]
+        public string ConfimacaoSenha { get; set; }
+
+    }
+}
